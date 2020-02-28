fe48b345001266fb8618a70da06b0c3474781e44
Pat O'Callaghan <pat@intercom.io>
2020-02-28 21:51:03 +0000

    Add failing test for action name issue

---
+++ tests/integration/components/my-component-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | my-component', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+
+    this.set('action', { foo: 'bar' });
+    await render(hbs`<MyComponent @action={{this.action}} />`);
+
+    assert.dom('[data-something="bar"]').exists();
+    assert.dom('[data-something="bar"]').hasText('bar');
+  });
+});
